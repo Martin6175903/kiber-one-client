05e695c3f39fc40e182284130b6edb5ddc7decc0
Martin6175903 <martin6175903@yandex.ru>
2025-03-21 14:39:42 +0300

    dded files and added additional settings

---
+++ src/constants/seo.constants.ts
@@ -0,0 +1,5 @@
+export const NO_INDEX_PAGE = {
+	robots: { index: false, follow: false }
+}
+export const SITE_NAME = 'Kiber-one'
+export const SITE_DESCRIPTION = 'Добро пожаловать в наш интернет-магазин - уникальную платформу для заказов КиберТоваров, созданную с учётом современных тенденций и технологий.'
