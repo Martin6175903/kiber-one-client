78fa4531ce67d977cb7c321ad1d6af48b53ddc4d
Martin6175903 <martin6175903@yandex.ru>
2025-03-23 00:36:54 +0300

    added files

--- src/components/ui/ConfirmModal.tsx
+++ src/components/ui/ConfirmModal.tsx
@@ -1,4 +1,12 @@
 import { PropsWithChildren } from 'react'
+import { AlertDialog, AlertDialogContent, AlertDialogDescription, AlertDialogTrigger } from '@radix-ui/react-alert-dialog'
+import {
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle
+} from './AlertDialog'
 
 interface ConfirmModalProps {
 	handleClick: () => void
@@ -6,9 +14,21 @@ interface ConfirmModalProps {
 
 const ConfirmModal = ({children, handleClick}: PropsWithChildren<ConfirmModalProps>) => {
 	return (
-		<div>
-			
-		</div>
+		<AlertDialog>
+			<AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Вы уверены?</AlertDialogTitle>
+					<AlertDialogDescription>
+						Это действие нельзя будет отменить.
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel>Закрыть</AlertDialogCancel>
+					<AlertDialogAction className={'bg-blue-500 hover:bg-blue-500/90'} onClick={() => handleClick()}>Подтвердить</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
 	)
 }
 
