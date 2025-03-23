3c78ecbfa1f51e9e38cc57b91dac83b313a3da93
Martin6175903 <martin6175903@yandex.ru>
2025-03-23 20:12:26 +0300

    dded shadcn ui components

---
+++ src/components/ui/data-loading/DataTableLoading.tsx
@@ -0,0 +1,21 @@
+import { Skeleton } from '../Skeleton'
+import { Card, CardContent } from '@/src/components/ui/Card'
+import Loader from '../Loader'
+
+const DataTableLoading = () => {
+	return (
+		<div className={'max-w-screen-2xl mx-auto w-full'}>
+			<Skeleton className={'h-8 w-48'}/>
+			<Skeleton className={'flex items-center py-4'}/>
+			<Card className={'rounded-md border mt-6'}>
+				<CardContent>
+					<div className={'h-[520px] w-full flex items-center justify-center'}>
+						<Loader/>
+					</div>
+				</CardContent>
+			</Card>
+		</div>
+	)
+}
+
+export default DataTableLoading
