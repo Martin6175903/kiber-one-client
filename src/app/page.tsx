615bbbab01a750d7f9fd0feb290cf8131bd02239
Martin6175903 <martin6175903@yandex.ru>
2025-03-27 00:39:54 +0300

    add footer in project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Header from '@/src/components/layouts/main-layout/header/Header'
 import Review from '@/src/components/layouts/main-layout/review/Review'
 import ProductsInfo from '@/src/components/layouts/main-layout/products/products-info/ProductsInfo'
 import Delivery from '@/src/components/layouts/main-layout/delivery/Delivery'
+import Footer from '@/src/components/layouts/main-layout/footer/Footer'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Review/>
       <ProductsInfo/>
       <Delivery/>
+      <Footer/>
     </div>
   );
 }
