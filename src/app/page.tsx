bb1dcf8d09a7528fa8eb7f684576e5ca0da98d33
Martin6175903 <martin6175903@yandex.ru>
2025-03-27 00:04:36 +0300

    add section products-info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from '@/src/components/layouts/main-layout/header/Header'
 import Review from '@/src/components/layouts/main-layout/review/Review'
+import ProductsInfo from '@/src/components/layouts/main-layout/products/products-info/ProductsInfo'
 
 export default function Home() {
   return (
-    <div className={'wrapper'}>
+    <div className={'wrapper bg-[#181818]'}>
       <Header/>
       <Review/>
+      <ProductsInfo/>
     </div>
   );
 }
---
+++ src/components/layouts/main-layout/products/products-info/ProductsInfo.tsx
@@ -0,0 +1,13 @@
+const ProductsInfo = () => {
+	return (
+		<section className={'text-center py-35 text-white'}>
+			<div className="container">
+				<h3 className={'text-3xl max-w-[635px] mx-auto'}>Яркие, стильные КиберТовары, созданные с любовью.</h3>
+				<p className={'text-3xl my-20'}>Для Вашего удобства рекомендуем отдельно оформлять заявку на товар из наличия и отдельно на товар из предзаказа. В ином случае заказ будет отправлен, когда все товары из заявки поступят на склад.</p>
+				<h2 className={'uppercase text-6xl font-bold bg-clip-text text-transparent bg-linear-[-86deg,#282828_0%,#E5CBCB_37%,#FCE24E_47%,#E4DEDE_54%,#FCE24E_66%,#282828_100%]'}>KiberShop</h2>
+			</div>
+		</section>
+	)
+}
+
+export default ProductsInfo
