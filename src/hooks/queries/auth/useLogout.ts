126a871d183223b403e3b7f52cb81d5cd3cb4a32
mokiros <mokiros@mokiros.dev>
2025-05-22 21:24:11 +0300

    refactor: Use `PUBLIC_URL` for logout redirection

--- src/hooks/queries/auth/useLogout.ts
+++ src/hooks/queries/auth/useLogout.ts
@@ -2,6 +2,7 @@ import { useMutation } from '@tanstack/react-query'
 import { authService } from '@/src/services/auth/auth.service'
 import { useRouter } from 'next/navigation'
 import { useMemo } from 'react'
+import { PUBLIC_URL } from '@/src/config/url.config'
 
 export const useLogout = () => {
   const router = useRouter()
@@ -9,7 +10,7 @@ export const useLogout = () => {
   const { mutate: logout, isPending } = useMutation({
     mutationKey: ['logout'],
     mutationFn: () => authService.logout(),
-    onSuccess: () => router.push('/auth')
+    onSuccess: () => router.push(PUBLIC_URL.auth())
   })
 
   return useMemo(() => ({
